dc37010ee599fc259a8fcbdfc271cbabf526508f
710490 <710490@DESKTOP-AC0KD4F.lan>
2020-07-03 17:58:53 +0200

    versione finale

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -31,17 +31,53 @@ public class FXMLController {
 
     @FXML
     private Button btnReset;
+    
+    @FXML 
+    private Button btnCancella;
+    
+    @FXML
+    private TextArea txtPerformance;
 
     @FXML
     void doInsert(ActionEvent event) {
-    	// TODO
+    	String parola = txtParola.getText();
+    	double inizio = System.nanoTime();
+    	elenco.addParola(parola);
+    	double fine = System.nanoTime();
+    	txtResult.clear();
+    	String result = "";
+    	for(String s : elenco.getElenco())
+    		result += s + "\n";
+    	txtResult.setText(result);
+    	txtPerformance.clear();
+    	txtPerformance.setText("Tempo impiegato: " + ((fine-inizio)/1e9) + " secondi.");
     }
 
     @FXML
     void doReset(ActionEvent event) {
-    	// TODO
+    	double inizio = System.nanoTime();
+    	elenco.reset();
+    	double fine = System.nanoTime();
+    	txtResult.clear();
+    	txtPerformance.clear();
+    	txtPerformance.setText("Tempo impiegato: " + ((fine-inizio)/1e9) + " secondi.");
     }
 
+    @FXML
+    void doCancella() {
+    	String selected = txtResult.getSelectedText();
+    	double inizio = System.nanoTime();
+    	elenco.cancella(selected);
+    	double fine = System.nanoTime();
+    	txtResult.clear();
+    	String result = "";
+    	for(String s : elenco.getElenco())
+    		result += s + "\n";
+    	txtResult.setText(result);
+    	txtPerformance.clear();
+    	txtPerformance.setText("Tempo impiegato: " + ((fine-inizio)/1e9) + " secondi.");
+    }
+    
     @FXML
     void initialize() {
         assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,36 @@
 package it.polito.tdp.parole.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Parole {
 		
+	List<String> parole; 
+	
 	public Parole() {
-		//TODO
+		//Costruttore 
+		parole = new ArrayList<String>();
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		//Il metodo permette di aggiungere la stringa p alla lista di stringhe
+		//p è la stringa da aggiungere
+		parole.add(p);
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		//Il metodo permette di ottenere la lista di stringhe ordinate seguendo l'ordine alfabetico
+		Collections.sort(parole);
+		return parole;
 	}
 	
 	public void reset() {
-		// TODO
+		//Il metodo permette di eliminare tutte le stringhe della lista
+		parole.clear();
 	}
 
+	public void cancella(String s) {
+		parole.remove(s);
+	}
 }
